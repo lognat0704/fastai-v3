e2bd0d270995768dfe5643e7e3335e5e9d6cf2dc
Nelson Hong Ming Tseng <hmtseng0704@gmail.com>
2019-02-06 10:02:17 +0800

    upload
    upload

--- app/server.py
+++ app/server.py
@@ -8,10 +8,10 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=ttps://drive.google.com/open?id=1xPfdoM7254IEDOWB5zFC5yd9mMZ5GYp'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['corgi', 'akita', 'shiba']
 path = Path(__file__).parent
 
 app = Starlette()
